9ee3b33659643ec8e84c4fa2c90c4121ba9118c3
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-06-10 15:20:45 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Task } from './models/task.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+tasks: Task[] = [
+  new Task('Epicodus hw',3),
+  new Task('Dinner',2),
+  new Task('Cleaning',2)
+];
+  selectedTask =null;
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
+  priorityColor(currentTask){
+  if (currentTask.priority === 3){
+    return "bg-danger";
+  } else if (currentTask.priority === 2) {
+    return  "bg-warning";
+  } else {
+    return "bg-info";
+  }
+}
+}
